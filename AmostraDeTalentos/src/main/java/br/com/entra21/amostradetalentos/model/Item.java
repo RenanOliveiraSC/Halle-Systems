b0ec9ca14391f47ca03538a465b3548c91cab8db
Noturno <Noturno@192.168.7.189>
2017-08-31 20:31:26 -0300

    Commit

--- AmostraDeTalentos/src/main/java/br/com/entra21/amostradetalentos/model/Item.java
+++ AmostraDeTalentos/src/main/java/br/com/entra21/amostradetalentos/model/Item.java
@@ -2,4 +2,5 @@ package br.com.entra21.amostradetalentos.model;
 
 public class Item extends Item_Servico{
 
+	
 }
--- AmostraDeTalentos/src/main/java/br/com/entra21/amostradetalentos/model/Servico.java
+++ AmostraDeTalentos/src/main/java/br/com/entra21/amostradetalentos/model/Servico.java
@@ -2,4 +2,5 @@ package br.com.entra21.amostradetalentos.model;
 
 public class Servico extends Item_Servico{
 
+	
 }
