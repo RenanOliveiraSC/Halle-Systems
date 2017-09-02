4afaf40c5c18e746293731a7d6abf5e90e53f941
Noturno <Noturno@192.168.7.189>
2017-09-01 21:56:57 -0300

    Commit

--- AmostraDeTalentos/src/main/java/br/com/entra21/amostradetalentos/model/Fabricante.java
+++ AmostraDeTalentos/src/main/java/br/com/entra21/amostradetalentos/model/Fabricante.java
@@ -2,4 +2,38 @@ package br.com.entra21.amostradetalentos.model;
 
 public class Fabricante {
 
+	private Integer codigo;
+	private String nome;
+
+	public Fabricante(Integer codigo, String nome) {
+		super();
+		this.codigo = codigo;
+		this.nome = nome;
+	}
+
+	public Fabricante(String nome) {
+		super();
+		this.nome = nome;
+	}
+
+	public Fabricante() {
+		super();
+	}
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
 }
