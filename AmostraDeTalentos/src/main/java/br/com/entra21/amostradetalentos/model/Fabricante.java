cfd8738192ebb6c72f365626065db06eed796159
Noturno <Noturno@192.168.7.12>
2017-09-01 21:56:13 -0300

    model fabricante ok

--- AmostraDeTalentos/src/main/java/br/com/entra21/amostradetalentos/model/Fabricante.java
+++ AmostraDeTalentos/src/main/java/br/com/entra21/amostradetalentos/model/Fabricante.java
@@ -2,4 +2,29 @@ package br.com.entra21.amostradetalentos.model;
 
 public class Fabricante {
 
+	private int codigo;
+	private String nome;
+	
+	public Fabricante (int codigo, String nome) {
+		this.codigo = codigo;
+		this.nome = nome;
+	}
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
 }
