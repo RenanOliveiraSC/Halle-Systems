a0da8c1bdc5fa3da06733b6f958ca4b9e6f7ff92
Noturno <Noturno@192.168.7.189>
2017-09-06 19:14:18 -0300

    Commit das 7 e 14

--- AmostraDeTalentos/src/main/java/br/com/entra21/amostradetalentos/service/CidadeService.java
+++ AmostraDeTalentos/src/main/java/br/com/entra21/amostradetalentos/service/CidadeService.java
@@ -1,5 +1,37 @@
 package br.com.entra21.amostradetalentos.service;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+import br.com.entra21.amostradetalentos.dao.CidadeDAO;
+import br.com.entra21.amostradetalentos.jdbc.oracle.ConnectionPoolOracle;
+import br.com.entra21.amostradetalentos.model.Cidade;
+import br.com.entra21.amostradetalentos.model.Estado;
+
 public class CidadeService {
 
+	public void inserir(String nome, Estado estado) throws SQLException {
+		try (Connection con = new ConnectionPoolOracle().getConnection()) {
+			new CidadeDAO(con).inserir(nome, estado);
+		}
+	}
+
+	public void alterar(Integer codigo, String nome) throws SQLException {
+		try (Connection con = new ConnectionPoolOracle().getConnection()) {
+			new CidadeDAO(con).alterar(codigo, nome);
+		}
+	}
+
+	public void excluir(Integer codigo) throws SQLException {
+		try (Connection con = new ConnectionPoolOracle().getConnection()) {
+			new CidadeDAO(con).excluir(codigo);
+		}
+	}
+
+	public List<Cidade> listarCachorros() throws SQLException {
+		try (Connection con = new ConnectionPoolOracle().getConnection()) {
+			return new CidadeDAO(con).lista();
+		}
+	}
 }
--- AmostraDeTalentos/src/main/java/br/com/entra21/amostradetalentos/service/EstadoService.java
+++ AmostraDeTalentos/src/main/java/br/com/entra21/amostradetalentos/service/EstadoService.java
@@ -1,5 +1,36 @@
 package br.com.entra21.amostradetalentos.service;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+import br.com.entra21.amostradetalentos.dao.EstadoDAO;
+import br.com.entra21.amostradetalentos.jdbc.oracle.ConnectionPoolOracle;
+import br.com.entra21.amostradetalentos.model.Estado;
+
 public class EstadoService {
 
+	public void inserir(String nome, String sigla) throws SQLException {
+		try (Connection con = new ConnectionPoolOracle().getConnection()) {
+			new EstadoDAO(con).inserir(nome, sigla);
+		}
+	}
+
+	public void alterar(Integer codigo, String nome) throws SQLException {
+		try (Connection con = new ConnectionPoolOracle().getConnection()) {
+			new EstadoDAO(con).alterar(codigo, nome);
+		}
+	}
+
+	public void excluir(Integer codigo) throws SQLException {
+		try (Connection con = new ConnectionPoolOracle().getConnection()) {
+			new EstadoDAO(con).excluir(codigo);
+		}
+	}
+
+	public List<Estado> listarCachorros() throws SQLException {
+		try (Connection con = new ConnectionPoolOracle().getConnection()) {
+			return new EstadoDAO(con).lista();
+		}
+	}
 }
