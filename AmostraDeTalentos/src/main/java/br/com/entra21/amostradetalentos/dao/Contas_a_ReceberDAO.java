22d80aa15d169129fe0bc51ac85847c10de6625c
Renan <Renan@SAPDELL>
2017-09-11 19:46:36 -0300

    contas a receber - Renan

--- AmostraDeTalentos/src/main/java/br/com/entra21/amostradetalentos/dao/Contas_a_ReceberDAO.java
+++ AmostraDeTalentos/src/main/java/br/com/entra21/amostradetalentos/dao/Contas_a_ReceberDAO.java
@@ -5,8 +5,8 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+import br.com.entra21.amostradetalentos.model.Caixa;
 import br.com.entra21.amostradetalentos.model.Contas_a_Receber;
-import br.com.entra21.amostradetalentos.model.FormaDePagamento;
 
 public class Contas_a_ReceberDAO {
 
@@ -16,17 +16,15 @@ public class Contas_a_ReceberDAO {
 		this.con = con;
 	}
 
-	public boolean inserir(Contas_a_Receber aReceber, FormaDePagamento formadepagamento) throws SQLException {
-		String sql = "INSERT INTO Contas_a_Pagar(CAP_CODIGO, CAP_COD_FORNECEDOR, CAP_COD_TIPO_PAGAMENTO, CAP_DATA_LANCAMENTO, CAP_DIAS_DE_ATRASO, CAP_TOTAL, CAP_SALDO, CAP_DESCONTO, CAP_PAGAMENTO_TOTAL) VALUES (SEQ_Contas_a_Pagar.NEXTVAL, ?,?,?,?,?,?,?,?,?)";
+	public boolean inserir(Contas_a_Receber aReceber, Caixa caixa) throws SQLException {
+		String sql = "INSERT INTO Contas_a_Pagar(CAR_CODIGO, CAR_CX_CODIGO, CAR_DATA_LANCAMENTO, CAR_DIAS_DE_ATRASO, CAR_SALDO, CAR_DECONTO, CAR_PAGAMENTO_TOTAL) VALUES (SEQ_Contas_a_Pagar.NEXTVAL, ?,?,?,?,?,?,?,?)";
 		PreparedStatement statement = con.prepareStatement(sql);
-		statement.setInt(1, aPagar.getFornecedor().getCodigo());
-		statement.setInt(2, aPagar.getForneceFormaDePagamentodor().getCodigo());
-		statement.setDate(3, (Date) aPagar.getDataLancamento());
-		statement.setInt(4, aPagar.getDiasAtraso());
-		statement.setDouble(5, aPagar.getTotal());
-		statement.setDouble(6, aPagar.getSaldo());
-		statement.setDouble(7, aPagar.getDesconto());
-		statement.setDouble(8, aPagar.getPagamentoTotal());
+		statement.setInt(1, aReceber.getCaixa().getCodigo());
+		statement.setDate(2, (Date) aReceber.getDataLancamento());
+		statement.setInt(3, aReceber.getDias_de_atraso());
+		statement.setDouble(4, aReceber.getSaldo());
+		statement.setDouble(5, aReceber.getDesconto());
+		statement.setDouble(6, aReceber.getPagamentoTotal());
 		return statement.executeUpdate() > 0;
 	}
 }
