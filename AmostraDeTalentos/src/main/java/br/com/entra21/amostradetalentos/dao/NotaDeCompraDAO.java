e5ee605a2c23cbc1f953dec676b066ff7c928b78
Renan <Renan@SAPDELL>
2017-09-12 19:26:17 -0300

    commit nota de compra - Renana / Lucas H. vai fazer a lista

--- AmostraDeTalentos/src/main/java/br/com/entra21/amostradetalentos/dao/NotaDeCompraDAO.java
+++ AmostraDeTalentos/src/main/java/br/com/entra21/amostradetalentos/dao/NotaDeCompraDAO.java
@@ -9,8 +9,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.com.entra21.amostradetalentos.model.Estado;
+import br.com.entra21.amostradetalentos.model.FormaDePagamento;
+import br.com.entra21.amostradetalentos.model.Fornecedor;
 import br.com.entra21.amostradetalentos.model.NotaDeCompra;
 import br.com.entra21.amostradetalentos.model.Pais;
+import br.com.entra21.amostradetalentos.model.Produto;
 
 public class NotaDeCompraDAO {
 
@@ -19,37 +22,50 @@ public class NotaDeCompraDAO {
 	public NotaDeCompraDAO(Connection con) {
 		this.con = con;
 	}
-	
-	public boolean inserir(NotaDeCompra notaDeCompra) throws SQLException {
-		String sql = "INSERT INTO NOTA_DE_COMPRA (NTC_CODIGO, NTC_QTDE, NTC_PRECO_ITEM, NTC_VALOR_TOTAL, NTC_DATA_LANCAMENTO, NTC_OBSERVACAO, NTC_ANEXO, NTC_NUMERO_NOTA, NTC_DESCONTO, NTC_FOR_CODIGO, NTC_PRO_CODIGO, NTC_FORMA_CODIGO) VALUES (SEQ_ESTADO.NEXTVAL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
 
+	public boolean inserir(NotaDeCompra notaDeCompra, Fornecedor fornecedor, Produto produto,
+			FormaDePagamento formaPagamento) throws SQLException {
+		String sql = "INSERT INTO NOTA_DE_COMPRA (NTC_CODIGO, NTC_FOR_CODIGO, NTC_PRO_CODIGO, NTC_QTDE, NTC_PRECO_PRODUTO, NTC_DESCONTO, NTC_VALOR_TOTAL, NTC_NUMERO_NOTA, NTC_DATA_LANCAMENTO, NTC_OBSERVACAO, NTC_ANEXO, NTC_FORMA_CODIGO) VALUES (SEQ_ESTADO.NEXTVAL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 		PreparedStatement statement = con.prepareStatement(sql);
 		statement.setInt(1, notaDeCompra.getCodigo());
-		statement.setDouble(2, notaDeCompra.getQuantidade_item());
-		statement.setDouble(3, notaDeCompra.getPrecoProduto());
-		statement.setDouble(4, notaDeCompra.getTotal());
-		statement.setDate(5, (Date)notaDeCompra.getData_lancamento());
-		statement.setString(6, notaDeCompra.getObs());
-		statement.setString(7, notaDeCompra.getAnexo());
-		statement.setString(8, notaDeCompra.getNum_da_nota());
-		
-		
-		
+		statement.setInt(2, notaDeCompra.getFornecedor().getCodigo());
+		statement.setInt(3, notaDeCompra.getProduto().getCodigo());
+		statement.setDouble(4, notaDeCompra.getQuantidade_produto());
+		statement.setDouble(5, notaDeCompra.getPrecoProduto());
+		statement.setDouble(6, notaDeCompra.getDesconto());
+		statement.setDouble(7, notaDeCompra.getPrecoProduto());
+		statement.setDouble(8, notaDeCompra.getValorTotal());
+		statement.setString(9, notaDeCompra.getNum_da_nota());
+		statement.setDate(10, (Date) notaDeCompra.getData_lancamento());
+		statement.setString(11, notaDeCompra.getObs());
+		statement.setString(12, notaDeCompra.getAnexo());
+		statement.setInt(13, notaDeCompra.getFormaDePagamento().getCodigo());
+
 		return statement.executeUpdate() > 0;
 	}
 
-	public boolean alterar(int id, String nome) throws SQLException {
-		String sql = "UPDATE ESTADO SET EST_NOME = ? WHERE EST_CODIGO = ?";
+	public boolean alterarForncedor(int id, String fornecedor) throws SQLException {
+		String sql = "UPDATE NOTA_DE_COMPRA SET NTC_FOR_CODIGO = ? WHERE NTC_CODIGO = ?";
 
 		PreparedStatement statement = con.prepareStatement(sql);
-		statement.setString(1, nome);
+		statement.setString(1, fornecedor);
+		statement.setInt(2, id);
+
+		return statement.executeUpdate() > 0;
+	}
+
+	public boolean alterarObservacao(int id, String observacao) throws SQLException {
+		String sql = "UPDATE NOTA_DE_COMPRA SET NTC_OBSERVACAO = ? WHERE NTC_CODIGO = ?";
+
+		PreparedStatement statement = con.prepareStatement(sql);
+		statement.setString(1, observacao);
 		statement.setInt(2, id);
 
 		return statement.executeUpdate() > 0;
 	}
 
 	public boolean excluir(int id) throws SQLException {
-		String sql = "DELETE ESTADO WHERE EST_CODIGO = ?";
+		String sql = "DELETE NOTA_DE_COMPRA WHERE NTC_CODIGO = ?";
 
 		PreparedStatement statement = con.prepareStatement(sql);
 		statement.setInt(1, id);
