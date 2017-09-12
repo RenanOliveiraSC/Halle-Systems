d5bae10669a1c6ccfc02177c177838afbe7b360d
Noturno <Noturno@192.168.7.12>
2017-09-12 19:21:06 -0300

    Lancamento contabil model

--- AmostraDeTalentos/src/main/java/br/com/entra21/amostradetalentos/model/LancamentoContabil.java
+++ AmostraDeTalentos/src/main/java/br/com/entra21/amostradetalentos/model/LancamentoContabil.java
@@ -5,16 +5,18 @@ import java.util.Date;
 public class LancamentoContabil {
 
 	private int codigo;
-	private String contasAReceberCodigo;
-	private String contasAPagarCodigo;
+	private Contas_a_Pagar contasAPagar;
+	private Contas_a_Receber contasAReceber;
 	private Date dataLancamento;
 	private String observacao;
-	public LancamentoContabil(int codigo, String contasAReceberCodigo, String contasAPagarCodigo, Date dataLancamento,
+	
+	
+	public LancamentoContabil(int codigo, Contas_a_Receber contasAReceber, Contas_a_Pagar contasAPagar, Date dataLancamento,
 			String observacao) {
 		super();
 		this.codigo = codigo;
-		this.contasAReceberCodigo = contasAReceberCodigo;
-		this.contasAPagarCodigo = contasAPagarCodigo;
+		this.contasAReceber = contasAReceber;
+		this.contasAPagar = contasAPagar;
 		this.dataLancamento = dataLancamento;
 		this.observacao = observacao;
 	}
@@ -24,22 +26,22 @@ public class LancamentoContabil {
 	public void setCodigo(int codigo) {
 		this.codigo = codigo;
 	}
-	public String getCar_codigo() {
-		return contasAReceberCodigo;
-	}
-	public void setCar_codigo(String contasAReceberCodigo) {
-		this.contasAReceberCodigo = contasAReceberCodigo;
-	}
-	public String getCap_codigo() {
-		return contasAPagarCodigo;
-	}
-	public void setCap_codigo(String contasAPagarCodigo) {
-		this.contasAPagarCodigo = contasAPagarCodigo;
-	}
-	public Date getData_lancamento() {
+	public Date getDataLancamento() {
 		return dataLancamento;
 	}
-	public void setData_lancamento(Date dataLancamento) {
+	public Contas_a_Pagar getContasAPagar() {
+		return contasAPagar;
+	}
+	public void setContasAPagar(Contas_a_Pagar contasAPagar) {
+		this.contasAPagar = contasAPagar;
+	}
+	public Contas_a_Receber getContasAReceber() {
+		return contasAReceber;
+	}
+	public void setContasAReceber(Contas_a_Receber contasAReceber) {
+		this.contasAReceber = contasAReceber;
+	}
+	public void setDataLancamento(Date dataLancamento) {
 		this.dataLancamento = dataLancamento;
 	}
 	public String getObservacao() {
