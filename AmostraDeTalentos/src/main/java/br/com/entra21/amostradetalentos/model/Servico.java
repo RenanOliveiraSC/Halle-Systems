e5c7f622d37952f566367172fe504a08626dadc5
Noturno <Noturno@192.168.7.189>
2017-09-12 21:47:03 -0300

    Commite

---
+++ AmostraDeTalentos/src/main/java/br/com/entra21/amostradetalentos/model/Servico.java
@@ -0,0 +1,17 @@
+package br.com.entra21.amostradetalentos.model;
+
+public class Servico extends ProdutoServico {
+
+	public Servico() {
+		super();
+	}
+
+	public Servico(Integer codigo, String descricao, Familia familia, boolean ativo, ListaDePreco listaDePreco,
+			double preco, String nomeUnidadeDeCompra, double porUnidadeDeCompra, double qtdePorCompra,
+			String nomeUnidadeVenda, double porunidadeDeVenda, double qtdePorVenda, String observacao, String anexo) {
+
+		super(codigo, descricao, familia, ativo, listaDePreco, preco, nomeUnidadeDeCompra, porUnidadeDeCompra,
+				qtdePorCompra, nomeUnidadeVenda, porunidadeDeVenda, qtdePorVenda, observacao, anexo);
+	}
+
+}
