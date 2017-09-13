dbcf85d83d7a534af09ac102d5e94190d7b2a6fc
Renan <Renan@SAPDELL>
2017-09-13 19:52:19 -0300

    commit fornecedorService - Renan

--- AmostraDeTalentos/src/main/java/br/com/entra21/amostradetalentos/service/FornecedorService.java
+++ AmostraDeTalentos/src/main/java/br/com/entra21/amostradetalentos/service/FornecedorService.java
@@ -6,20 +6,68 @@ import java.sql.SQLException;
 import br.com.entra21.amostradetalentos.dao.FornecedorDAO;
 import br.com.entra21.amostradetalentos.jdbc.oracle.ConnectionPoolOracle;
 import br.com.entra21.amostradetalentos.model.Endereco;
+import br.com.entra21.amostradetalentos.model.Fornecedor;
 import br.com.entra21.amostradetalentos.model.TipoFornecedor;
 
 public class FornecedorService {
 
-	public void inserir(String nome, TipoFornecedor tipofornecedor, Endereco endereco) throws SQLException {
+	public void inserir(Fornecedor fornecedor, TipoFornecedor tipofornecedor, Endereco endereco) throws SQLException {
 		try (Connection con = new ConnectionPoolOracle().getConnection()) {
-			new FornecedorDAO(con).inserir(nome, tipofornecedor, endereco);
+			new FornecedorDAO(con).inserir(fornecedor, tipofornecedor, endereco);
 		}
 	}
 
-	public void alterar(Integer codigo, String nome) throws SQLException {
+	public void alterarNome(Integer id, String nome) throws SQLException {
 		try (Connection con = new ConnectionPoolOracle().getConnection()) {
-			new FornecedorDAO(con).alterarNome(codigo, nome);
+			new FornecedorDAO(con).alterarNome(id, nome);
 		}
 	}
 
+	public void alterarContato(Integer id, String contato) throws SQLException {
+		try (Connection con = new ConnectionPoolOracle().getConnection()) {
+			new FornecedorDAO(con).alterarNome(id, contato);
+		}
+	}
+
+	public void alterarCnpj(Integer id, String cnpj) throws SQLException {
+		try (Connection con = new ConnectionPoolOracle().getConnection()) {
+			new FornecedorDAO(con).alterarNome(id, cnpj);
+		}
+	}
+
+	public void alterarTelefone(Integer id, String telefone) throws SQLException {
+		try (Connection con = new ConnectionPoolOracle().getConnection()) {
+			new FornecedorDAO(con).alterarNome(id, telefone);
+		}
+	}
+
+	public void alterarCelular(Integer id, String celular) throws SQLException {
+		try (Connection con = new ConnectionPoolOracle().getConnection()) {
+			new FornecedorDAO(con).alterarNome(id, celular);
+		}
+	}
+
+	public void alterarEmail(Integer id, String email) throws SQLException {
+		try (Connection con = new ConnectionPoolOracle().getConnection()) {
+			new FornecedorDAO(con).alterarNome(id, email);
+		}
+	}
+
+	public void alterarTipoFornecedor(Integer id, String tipoFornecedor) throws SQLException {
+		try (Connection con = new ConnectionPoolOracle().getConnection()) {
+			new FornecedorDAO(con).alterarNome(id, tipoFornecedor);
+		}
+	}
+
+	public void alterarEndereco(Integer id, String endereco) throws SQLException {
+		try (Connection con = new ConnectionPoolOracle().getConnection()) {
+			new FornecedorDAO(con).alterarNome(id, endereco);
+		}
+	}
+
+	public void excluir(Integer id) throws SQLException {
+		try (Connection con = new ConnectionPoolOracle().getConnection()) {
+			new FornecedorDAO(con).excluir(id);
+		}
+	}
 }
