9ec5864ce4c885f4369c5a69777ca70389872897
Renan <Renan@SAPDELL>
2017-09-12 21:25:25 -0300

    commit TabelasIntermediariasDAO - Renan

---
+++ AmostraDeTalentos/src/main/java/br/com/entra21/amostradetalentos/dao/Servico_ProdutoDAO.java
@@ -0,0 +1,12 @@
+package br.com.entra21.amostradetalentos.dao;
+
+import java.sql.Connection;
+
+public class Servico_ProdutoDAO {
+
+	private final Connection con;
+
+	public Servico_ProdutoDAO(Connection con) {
+		this.con = con;
+	}
+}
