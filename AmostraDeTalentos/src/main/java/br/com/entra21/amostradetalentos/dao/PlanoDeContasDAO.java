df1bb095250f9a92e27298d9d660369b593c1532
Noturno <Noturno@192.168.7.10>
2017-09-13 19:22:03 -0300

    PlanodecontasDAO terminado

--- AmostraDeTalentos/src/main/java/br/com/entra21/amostradetalentos/dao/PlanoDeContasDAO.java
+++ AmostraDeTalentos/src/main/java/br/com/entra21/amostradetalentos/dao/PlanoDeContasDAO.java
@@ -2,7 +2,10 @@ package br.com.entra21.amostradetalentos.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import br.com.entra21.amostradetalentos.model.PlanoDeContas;
 
@@ -17,6 +20,7 @@ public class PlanoDeContasDAO {
 	public boolean inserir(PlanoDeContas planodecontas) throws SQLException {
 		String sql = "INSERT INTO PLANO_DE_CONTAS (CONTA_CODIGO, CONTA_NOME, CONTA_SALDO, CONTA_CAIXA, CONTA_ATIVA, CONTA_CONFIDENCIAL, CONTA_CENTRO_DE_CUSTO, CONTA_CODIGO_EXTERNO) VALUES (SEQ_CONTA.NEXTVAL, ?, ?, ?, ?, ?, ?, ?);";
 		PreparedStatement statement = con.prepareStatement(sql);
+		/*------------------------------------------------------*/
 		statement.setString(1, planodecontas.getNome_conta());
 		statement.setInt(2, planodecontas.getSaldo());
 		statement.setString(3, planodecontas.getConta_caixa());
@@ -27,4 +31,84 @@ public class PlanoDeContasDAO {
 		
 		return statement.executeUpdate() > 0;
 	}
+	
+	public boolean alterarNomeconta(int id, String nomeconta) throws SQLException {
+		String sql = "UPDATE PLANO_DE_CONTAS SET CONTA_NOME = ? WHERE CONTA_CODIGO = ?";
+
+		PreparedStatement statement = con.prepareStatement(sql);
+		statement.setString(1, nomeconta);
+		statement.setInt(2, id);
+
+		return statement.executeUpdate() > 0;
+	}
+	
+	public boolean alterarContaativa(int id, String contaativa) throws SQLException {
+		String sql = "UPDATE PLANO_DE_CONTAS SET CONTA_ATIVA = ? WHERE CONTA_CODIGO = ?";
+
+		PreparedStatement statement = con.prepareStatement(sql);
+		statement.setString(1, contaativa);
+		statement.setInt(2, id);
+
+		return statement.executeUpdate() > 0;
+	}
+	
+	public boolean alterarContaconfidencial(int id, String contaconfidencial) throws SQLException {
+		String sql = "UPDATE PLANO_DE_CONTAS SET CONTA_CONFIDENCIAL = ? WHERE CONTA_CODIGO = ?";
+
+		PreparedStatement statement = con.prepareStatement(sql);
+		statement.setString(1, contaconfidencial);
+		statement.setInt(2, id);
+
+		return statement.executeUpdate() > 0;
+	}
+	
+	public boolean alterarCentrocusto(int id, String centrocusto) throws SQLException {
+		String sql = "UPDATE PLANO_DE_CONTAS SET CONTA_CENTRO_DE_CUSTO = ? WHERE CONTA_CODIGO = ?";
+
+		PreparedStatement statement = con.prepareStatement(sql);
+		statement.setString(1, centrocusto);
+		statement.setInt(2, id);
+
+		return statement.executeUpdate() > 0;
+	}
+	
+	public boolean alterarCodigoexterno(int id, String codigoexterno) throws SQLException {
+		String sql = "UPDATE PLANO_DE_CONTAS SET CONTA_CODIGO_EXTERNO = ? WHERE CONTA_CODIGO = ?";
+
+		PreparedStatement statement = con.prepareStatement(sql);
+		statement.setString(1, codigoexterno);
+		statement.setInt(2, id);
+
+		return statement.executeUpdate() > 0;
+	}
+	
+	
+	public List<PlanoDeContas> lista() throws SQLException {
+		List<PlanoDeContas> lplanodecontas = new ArrayList<>();
+
+		String sql = "select * from PLANO_DE_CONTAS";
+		try (PreparedStatement stmt = con.prepareStatement(sql)) {
+			stmt.execute();
+			try (ResultSet rs = stmt.getResultSet()) {
+				while (rs.next()) {
+					int id = rs.getInt("CONTA_CODIGO");
+					String nome = rs.getString("CONTA_NOME");
+					int saldo = rs.getInt("CONTA_SALDO");
+					String caixa = rs.getString("CONTA_CAIXA");
+					String ativa = rs.getString("CONTA_ATIVA");
+					String confidencial = rs.getString("CONTA_CONFIDENCIAL");
+					String centrodecusto = rs.getString("CENTRO_DE_CUSTO");
+					String codigoexterno = rs.getString("CONTA_CODIGO_EXTERNO");
+					
+					PlanoDeContas planodecontas = new PlanoDeContas(id, nome, saldo, caixa, ativa, confidencial, centrodecusto, codigoexterno);
+					lplanodecontas.add(planodecontas);
+				}
+			}
+		}
+
+		return lplanodecontas;
+
+	}
+	
+	
 }
