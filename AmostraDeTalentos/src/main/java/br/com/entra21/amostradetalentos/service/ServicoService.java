9a0dfb876db53637bd92c9929e973e96c908d0c5
Noturno <Noturno@192.168.7.12>
2017-09-14 19:00:28 -0300

    ServicoService

--- AmostraDeTalentos/src/main/java/br/com/entra21/amostradetalentos/service/ServicoService.java
+++ AmostraDeTalentos/src/main/java/br/com/entra21/amostradetalentos/service/ServicoService.java
@@ -1,5 +1,31 @@
 package br.com.entra21.amostradetalentos.service;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+import br.com.entra21.amostradetalentos.dao.ServicoDAO;
+import br.com.entra21.amostradetalentos.jdbc.oracle.ConnectionPoolOracle;
+import br.com.entra21.amostradetalentos.model.Servico;
+
 public class ServicoService {
+	
+	public void inserir(Servico servico) throws SQLException {
+		try (Connection con = new ConnectionPoolOracle().getConnection()) {
+			new ServicoDAO(con).inserir(servico);
+		}
+	}
+	
+	public void excluir(int id) throws SQLException {
+		try (Connection con = new ConnectionPoolOracle().getConnection()) {
+			new ServicoDAO(con).excluir(id);
+		}
+	}
+	
+	public List<Servico> lista() throws SQLException {
+		try (Connection con = new ConnectionPoolOracle().getConnection()) {
+			return new ServicoDAO(con).lista();
+		}
+	}
 
 }
