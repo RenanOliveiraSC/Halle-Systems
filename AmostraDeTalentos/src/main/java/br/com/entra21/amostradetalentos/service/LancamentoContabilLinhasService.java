3d388ca941ef6ae28dcb31621b220c9cd865bea8
Noturno <Noturno@192.168.7.12>
2017-09-14 19:25:18 -0300

    lancamentoContabil e ContabilLinhas Service

--- AmostraDeTalentos/src/main/java/br/com/entra21/amostradetalentos/service/LancamentoContabilLinhasService.java
+++ AmostraDeTalentos/src/main/java/br/com/entra21/amostradetalentos/service/LancamentoContabilLinhasService.java
@@ -1,5 +1,34 @@
 package br.com.entra21.amostradetalentos.service;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.SQLException;
+
+import br.com.entra21.amostradetalentos.dao.LancamentoContabilLinhasDAO;
+import br.com.entra21.amostradetalentos.jdbc.oracle.ConnectionPoolOracle;
+import br.com.entra21.amostradetalentos.model.ContasAPagar;
+import br.com.entra21.amostradetalentos.model.ContasAReceber;
+import br.com.entra21.amostradetalentos.model.LancamentoContabilLinhas;
+
 public class LancamentoContabilLinhasService {
+	
+	public void inserir(LancamentoContabilLinhas lancamentoLinha, ContasAPagar contaApagar,
+			ContasAReceber contasAreceber) throws SQLException {
+		try (Connection con = new ConnectionPoolOracle().getConnection()){
+			new LancamentoContabilLinhasDAO(con).inserir(lancamentoLinha, contaApagar, contasAreceber);
+		}
+	}
+	
+	public void alterarObservacao(int id, String observacao) throws SQLException {
+		try (Connection con = new ConnectionPoolOracle().getConnection()){
+			new LancamentoContabilLinhasDAO(con).alterarObservacao(id, observacao);
+		}
+	}
+	
+	public void alterarDataVencimento(int id, Date dataVencimento) throws SQLException {
+		try (Connection con = new ConnectionPoolOracle().getConnection()){
+			new LancamentoContabilLinhasDAO(con).alterarDataVencimento(id, dataVencimento);
+		}
+	}
 
 }
