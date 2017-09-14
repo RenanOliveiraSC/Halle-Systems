253b1a4919a4bb95fb14a6316e0ddbc677412794
Noturno <Noturno@192.168.7.12>
2017-09-13 21:18:42 -0300

    Contas a pagar e a receber service

--- AmostraDeTalentos/src/main/java/br/com/entra21/amostradetalentos/service/ContasAReceberService.java
+++ AmostraDeTalentos/src/main/java/br/com/entra21/amostradetalentos/service/ContasAReceberService.java
@@ -1,5 +1,19 @@
 package br.com.entra21.amostradetalentos.service;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import br.com.entra21.amostradetalentos.dao.ContasAReceberDAO;
+import br.com.entra21.amostradetalentos.jdbc.oracle.ConnectionPoolOracle;
+import br.com.entra21.amostradetalentos.model.Caixa;
+import br.com.entra21.amostradetalentos.model.ContasAReceber;
+
 public class ContasAReceberService {
+	
+	public void inserir(ContasAReceber aReceber, Caixa caixa) throws SQLException {
+		try (Connection con = new ConnectionPoolOracle().getConnection()){
+			new ContasAReceberDAO(con).inserir(aReceber, caixa);
+		}
+	}
 
 }
