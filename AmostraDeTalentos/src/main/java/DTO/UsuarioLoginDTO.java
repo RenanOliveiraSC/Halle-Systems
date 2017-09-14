6970cbeeaeb47c529f8e4c3e759e0e1e42f519c0
Renan <Renan@SAPDELL>
2017-09-14 18:16:27 -0300

    Commit DTO - Renan

---
+++ AmostraDeTalentos/src/main/java/DTO/UsuarioLoginDTO.java
@@ -0,0 +1,54 @@
+package DTO;
+
+import br.com.entra21.amostradetalentos.model.UsuarioLogin;
+
+public class UsuarioLoginDTO {
+
+	private Integer codigo;
+	private String login;
+	private String senha;
+
+	public UsuarioLoginDTO(Integer codigo, String login, String senha) {
+		this.codigo = codigo;
+		this.login = login;
+		this.senha = senha;
+	}
+
+	public UsuarioLoginDTO(String login, String senha) {
+		this.login = login;
+		this.senha = senha;
+	}
+
+	public UsuarioLoginDTO() {
+
+	}
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public UsuarioLogin toUsuarioLogin() {
+		return new UsuarioLogin(this.codigo, this.login, this.senha);
+	}
+
+}
