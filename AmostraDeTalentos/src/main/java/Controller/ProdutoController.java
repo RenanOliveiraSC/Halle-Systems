123d8eb2bb562fbec4a815847e05d5ba47eb4f40
Noturno <Noturno@192.168.7.189>
2017-09-14 21:52:34 -0300

    Commite

--- AmostraDeTalentos/src/main/java/Controller/ProdutoController.java
+++ AmostraDeTalentos/src/main/java/Controller/ProdutoController.java
@@ -1,5 +1,61 @@
 package Controller;
 
+import java.sql.SQLException;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import DTO.ProdutoDTO;
+import br.com.entra21.amostradetalentos.service.ProdutoService;
+
+@Path("Produto")
+
 public class ProdutoController {
 
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/")
+
+	public Response create(ProdutoDTO produto) {
+		ProdutoService produtoService = new ProdutoService();
+		try {
+			produtoService.inserir(produto.toProduto());
+			return Response.status(Response.Status.OK).build();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+		}
+	}
+
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/")
+	public Response update(ProdutoDTO produto) {
+		ProdutoService produtoService = new ProdutoService();
+		try {
+			produtoService.inserir(produto.toProduto());
+			return Response.status(Response.Status.OK).build();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+		}
+	}
+
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/")
+	public Response Delete(ProdutoDTO produto) {
+		ProdutoService produtoService = new ProdutoService();
+		try {
+			produtoService.inserir(produto.toProduto());
+			return Response.status(Response.Status.OK).build();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+		}
+	}
 }
