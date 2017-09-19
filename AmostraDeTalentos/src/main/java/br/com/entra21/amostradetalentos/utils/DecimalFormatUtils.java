efd1ab857e2d8e98701d5118c8a8d0b1ac0bc8f8
Noturno <Noturno@192.168.7.189>
2017-09-19 20:30:54 -0300

    Commite

--- AmostraDeTalentos/src/main/java/br/com/entra21/amostradetalentos/utils/DecimalFormatUtils.java
+++ AmostraDeTalentos/src/main/java/br/com/entra21/amostradetalentos/utils/DecimalFormatUtils.java
@@ -4,11 +4,19 @@ import java.text.DecimalFormat;
 
 public class DecimalFormatUtils {
 
+	static DecimalFormat df = new DecimalFormat("#0.00");
+
 	public static String formatDecimal(double n) {
-		DecimalFormat df = new DecimalFormat("#0.00");
 
 		String numFormatado = df.format(n);
 
 		return numFormatado;
+	}
+
+	public static double formatToDouble(String numeroS) {
+
+		double numeroD = Double.parseDouble(numeroS);
+
+		return numeroD;
 	}
 }
