ef274222d536f020e3c29a7dbb12622f98babe33
Noturno <Noturno@192.168.7.189>
2017-09-19 19:09:31 -0300

    Commite

--- AmostraDeTalentos/src/main/java/br/com/entra21/amostradetalentos/dto/ServicosDTO.java
+++ AmostraDeTalentos/src/main/java/br/com/entra21/amostradetalentos/dto/ServicosDTO.java
@@ -1,16 +1,29 @@
 package br.com.entra21.amostradetalentos.dto;
 
+import br.com.entra21.amostradetalentos.model.Familia;
+import br.com.entra21.amostradetalentos.model.ListaDePreco;
 import br.com.entra21.amostradetalentos.model.Servico;
 
-public class ServicosDTO {
+public class ServicosDTO extends ProdutoServicoDTO{
 
 	
+	
+	public ServicosDTO(Integer codigo, String descricao, Familia familia, boolean ativo, ListaDePreco listaDePreco,
+			double preco, String nomeUnidadeDeCompra, double porUnidadeDeCompra, double qtdePorCompra,
+			String nomeUnidadeVenda, double porUnidadeDeVenda, double qtdePorVenda, String observacao, String anexo) {
+		super(codigo, descricao, familia, ativo, listaDePreco, preco, nomeUnidadeDeCompra, porUnidadeDeCompra, qtdePorCompra,
+				nomeUnidadeVenda, porUnidadeDeVenda, qtdePorVenda, observacao, anexo);
+	}
+
+	public ServicosDTO(Integer codigo, String descricao) {
+		super(codigo, descricao);
+	}
+
 	public ServicosDTO() {
-		// TODO Auto-generated constructor stub
+
 	}
 	
 	public Servico toServico() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 	
