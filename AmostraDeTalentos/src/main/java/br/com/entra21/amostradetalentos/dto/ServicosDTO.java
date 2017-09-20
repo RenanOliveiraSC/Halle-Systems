2716ebe3e8349642434af5f093242b6718ff6e25
Noturno <Noturno@192.168.7.189>
2017-09-19 21:05:34 -0300

    Commite

--- AmostraDeTalentos/src/main/java/br/com/entra21/amostradetalentos/dto/ServicosDTO.java
+++ AmostraDeTalentos/src/main/java/br/com/entra21/amostradetalentos/dto/ServicosDTO.java
@@ -4,11 +4,13 @@ import br.com.entra21.amostradetalentos.model.Familia;
 import br.com.entra21.amostradetalentos.model.ListaDePreco;
 import br.com.entra21.amostradetalentos.model.Servico;
 
+import br.com.entra21.amostradetalentos.utils.*;
+
 public class ServicosDTO extends ProdutoServicoDTO {
 
 	public ServicosDTO(Integer codigo, String descricao, Familia familia, boolean ativo, ListaDePreco listaDePreco,
-			double preco, String nomeUnidadeDeCompra, double porUnidadeDeCompra, double qtdePorCompra,
-			String nomeUnidadeVenda, double porUnidadeDeVenda, double qtdePorVenda, String observacao, String anexo) {
+			String preco, String nomeUnidadeDeCompra, String porUnidadeDeCompra, double qtdePorCompra,
+			String nomeUnidadeVenda, String porUnidadeDeVenda, double qtdePorVenda, String observacao, String anexo) {
 		super(codigo, descricao, familia, ativo, listaDePreco, preco, nomeUnidadeDeCompra, porUnidadeDeCompra,
 				qtdePorCompra, nomeUnidadeVenda, porUnidadeDeVenda, qtdePorVenda, observacao, anexo);
 	}
@@ -23,8 +25,9 @@ public class ServicosDTO extends ProdutoServicoDTO {
 
 	public Servico toServico() {
 		return new Servico(this.getCodigo(), this.getDescricao(), this.getFamilia(), this.isAtivo(),
-				this.getListaDePreco(), this.getPreco(), this.getNomeUnidadeDeCompra(), this.getPorUnidadeDeCompra(),
-				this.getQtdePorCompra(), this.getNomeUnidadeVenda(), this.getPorUnidadeDeVenda(),
+				this.getListaDePreco(), DecimalFormatUtils.parseDouble(this.getPreco()), this.getNomeUnidadeDeCompra(),
+				DecimalFormatUtils.parseDouble(this.getPorUnidadeDeCompra()), this.getQtdePorCompra(),
+				this.getNomeUnidadeVenda(), DecimalFormatUtils.parseDouble(this.getPorUnidadeDeVenda()),
 				this.getQtdePorVenda(), this.getObservacao(), this.getAnexo());
 	}
 
